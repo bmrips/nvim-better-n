fc42839d496babeaa74feb7ef1f4d2e8b388ddaf
Benedikt Rips <benedikt.rips@gmail.com>
2025-12-05 12:50:22 +0100

    feat: validate arguments of exported functions

--- lua/better-n/motion.lua
+++ lua/better-n/motion.lua
@@ -25,6 +25,22 @@ end
 ---@field modes? string|string[] The modes in which the motion is available
 ---@field map_args? vim.keymap.set.Opts Mapping arguments
 
+local function validate(opts)
+  vim.validate("initiate", opts.initiate, { "string", "function" }, true)
+  vim.validate("next", opts.next, { "string", "function" })
+  vim.validate("previous", opts.previous, { "string", "function" })
+  vim.validate("modes", opts.modes, { "string", "table" }, true)
+  vim.validate("map_args", opts.map_args, "table", true)
+end
+
+local function defaults(opts)
+  return {
+    initiate = opts.next,
+    modes = { "n", "o", "x" },
+    map_args = {},
+  }
+end
+
 ---@class better-n.motion.config
 ---@field initiate string|fun():string? Action that initiates the motion
 ---@field next string|fun():string? Action that moves to the next occurrence
@@ -35,40 +51,37 @@ end
 ---@param opts better-n.motion.opts
 ---@return better-n.motion.config
 function M.create(opts)
-  local initiate = opts.initiate or opts.next or error("opts.next or opts.initiate is required" .. vim.inspect(opts))
-  local next = opts.next or error("opts.next is required" .. vim.inspect(opts))
-  local previous = opts.previous or error("opts.previous is required" .. vim.inspect(opts))
-  local modes = opts.modes or { "n", "o", "x" }
-  local map_args = opts.map_args or {}
+  validate(opts)
+  local cfg = vim.tbl_extend("keep", opts, defaults(opts))
 
-  if type(map_args.buffer) == "boolean" then
-    map_args.buffer = map_args.buffer and vim.api.nvim_get_current_buf() or nil
-  elseif map_args.buffer == 0 then
-    map_args.buffer = vim.api.nvim_get_current_buf()
+  if type(cfg.map_args.buffer) == "boolean" then
+    cfg.map_args.buffer = cfg.map_args.buffer and vim.api.nvim_get_current_buf() or nil
+  elseif cfg.map_args.buffer == 0 then
+    cfg.map_args.buffer = vim.api.nvim_get_current_buf()
   end
 
   -- If `map_args.remap` is set, unset it since we handle it here: for every
   -- action that is a key (i.e. a string), extract its right-hand side.
-  if map_args.remap then
-    map_args.remap = nil
-    local keymap = Keymap:new("n", map_args.buffer)
-    next = is_key(next) and keymap:rhs_of(next) or next
-    previous = is_key(previous) and keymap:rhs_of(previous) or previous
+  if cfg.map_args.remap then
+    cfg.map_args.remap = nil
+    local keymap = Keymap:new("n", cfg.map_args.buffer)
+    cfg.next = is_key(cfg.next) and keymap:rhs_of(cfg.next) or cfg.next
+    cfg.previous = is_key(cfg.previous) and keymap:rhs_of(cfg.previous) or cfg.previous
   end
 
   local function go(action)
     return function()
       M.reset_nN()
 
-      local n_map_args = vim.tbl_extend("force", map_args, { desc = "Repeat motion" })
-      vim.keymap.set(modes, "n", next, n_map_args)
+      local n_map_args = vim.tbl_extend("force", cfg.map_args, { desc = "Repeat motion" })
+      vim.keymap.set(cfg.modes, "n", cfg.next, n_map_args)
 
-      local N_map_args = vim.tbl_extend("force", map_args, { desc = "Repeat opposite motion" })
-      vim.keymap.set(modes, "<S-n>", previous, N_map_args)
+      local N_map_args = vim.tbl_extend("force", cfg.map_args, { desc = "Repeat opposite motion" })
+      vim.keymap.set(cfg.modes, "<S-n>", cfg.previous, N_map_args)
 
       scope = {
-        buffer = map_args.buffer,
-        modes = modes,
+        buffer = cfg.map_args.buffer,
+        modes = cfg.modes,
       }
 
       return type(action) == "function" and action() or action
@@ -77,14 +90,14 @@ function M.create(opts)
 
   -- If one of the actions is a key, set <expr> since the callback returned by
   -- `go()` returns that key.
-  local go_map_args = vim.deepcopy(map_args)
-  go_map_args.expr = map_args.expr or is_key(initiate) or is_key(next) or is_key(previous)
+  local go_map_args = vim.deepcopy(cfg.map_args)
+  go_map_args.expr = cfg.map_args.expr or is_key(cfg.initiate) or is_key(cfg.next) or is_key(cfg.previous)
 
   return {
-    initiate = go(initiate),
-    next = go(next),
-    previous = go(previous),
-    modes = modes,
+    initiate = go(cfg.initiate),
+    next = go(cfg.next),
+    previous = go(cfg.previous),
+    modes = cfg.modes,
     map_args = go_map_args,
   }
 end
